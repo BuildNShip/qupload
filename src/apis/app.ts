e063704eace0147a5ddbc04e840ae1c75e22aa4c
BlazeSN860 <virajprabhup@gmail.com>
2024-03-28 01:24:26 +0530

    feat(app): list file api added.

--- src/apis/app.ts
+++ src/apis/app.ts
@@ -3,38 +3,61 @@ import { Dispatch } from "react";
 import { toast } from "react-hot-toast";
 
 export const getUniqueName = (
-  setUniqueName: Dispatch<React.SetStateAction<string[]>>
-) => {
-  axios
-    .post(`${import.meta.env.VITE_BACKEND_URL}qupload/get-unique-name/`)
-    .then((response) => {
-      console.log(response.data);
-      setUniqueName((prevNames) => [...prevNames, response.data.response.unique_code]);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  setUniqueNames: Dispatch<React.SetStateAction<string[]>>
+): Promise<string | undefined> => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${import.meta.env.VITE_BACKEND_URL}qupload/get-unique-name/`)
+      .then((response) => {
+        console.log(response.data);
+        setUniqueNames((prevNames) => ([...prevNames, response.data.response.unique_code]));
+        resolve(response.data.response.unique_code);
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      })
+  }
+  )
 };
 
-export const uploadFile = (acceptedFile: File, uniqueName: string) => {
-  const formData = new FormData();
-  formData.append("file", acceptedFile);
-  formData.append("unique_name", uniqueName);
-  const loader = toast.loading("Uploading...");
-  axios
-    .post(`${import.meta.env.VITE_BACKEND_URL}qupload/files/`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    })
-    .then((response) => {
-      console.log(response.data);
-      toast.success(response.data.message.general);
-    })
-    .catch((error) => {
-      console.error(error);
-      toast.error(error);
-    }).finally(() => {
-      toast.dismiss(loader);
-    });
+export const uploadFile = (acceptedFile: File, uniqueName: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const formData = new FormData();
+    formData.append("file", acceptedFile);
+    formData.append("unique_name", uniqueName);
+    const loader = toast.loading("Uploading...");
+    axios
+      .post(`${import.meta.env.VITE_BACKEND_URL}qupload/files/`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        toast.success(response.data.message.general);
+        resolve();
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error);
+        reject(error);
+      })
+      .finally(() => {
+        toast.dismiss(loader);
+      });
+  });
 };
+
+export const listFile = (uniqueName: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}qupload/files/${uniqueName}`)
+      .then((response) => {
+        resolve(response.data.response.files[0]);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  })
+}
